dc68aba4674c0c2e3c2db2bd388b42c6941630ad
jin1995 <897073574@qq.com>
2019-10-28 16:49:46 +0800

    path fs. readFile

--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
 import * as http from 'http'
+import * as fs from 'fs';
+import * as p from 'path';
 
 const server = http.createServer();
+const publicDir = p.resolve(__dirname,'public')
 
 server.on('request', (request:http.IncomingMessage,response:http.ServerResponse)=>{
-  console.log('request.method', request.method);
-  console.log('request.url', request.url);
-  console.log('request.header', request.headers);
-  const array = []  
-  request.on('data',(chunk)=>{
-    array.push(chunk)
-    console.log('--', array);
-    
-  })
-  request.on('end', ()=>{
-    const body = Buffer.concat(array).toString()
-    console.log('body', body);
-    
-  })
+  const {method,url,headers} = request
+  switch(url){
+    case '/index.html':
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error,data)=>{
+        if(error) throw error;
+        response.end(data.toString())
+      })
+      break
+  }
+
 })
 
 server.listen(8888)
