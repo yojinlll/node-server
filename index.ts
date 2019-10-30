1d2732c224c25de04132b858f495e52d18a96adb
jin1995 <897073574@qq.com>
2019-10-30 16:30:31 +0800

    状态码处理 & cache control

--- index.ts
+++ index.ts
@@ -5,36 +5,38 @@ import * as url from 'url';
 
 const server = http.createServer();
 const publicDir = p.resolve(__dirname,'public')
+const cacheTime = 31536000
 
 server.on('request', (request:http.IncomingMessage,response:http.ServerResponse)=>{
   const {method,url:path,headers} = request
-  console.log(path,'-----path--');
   const {pathname,search} = url.parse(path)
-  switch(pathname){
-    case '/index.html':
-      fs.readFile(p.resolve(publicDir, 'index.html'), (error,data)=>{
-        if(error) throw error;
-        response.end(data.toString())
-      })
-      break
-    case '/style.css':
-      response.setHeader('Content-type','text/css; charset=utf-8')
-      fs.readFile(p.resolve(publicDir, 'style.css'), (error,data)=>{
-        if(error) throw error;
-        response.end(data.toString())
-      })
-      break
-    case '/index.js':
-      response.setHeader('Content-type','text/javascript; charset=utf-8')
-      fs.readFile(p.resolve(publicDir, 'index.js'), (error,data)=>{
-        if(error) throw error;
-        response.end(data.toString())
-      })
-      break
-    default:
-      response.statusCode = 404
-      response.end()
+  if(method !== 'GET'){
+    response.statusCode =405
+    response.end()
+    return
   }
+
+  let filename = pathname.substr(1)
+  filename === '' ? filename = 'index.html' : filename
+  
+  fs.readFile(p.resolve(publicDir, filename), (error,data)=>{
+    if(error){
+      if(error.errno === -4058){
+        response.statusCode = 404
+        fs.readFile(p.resolve(publicDir, '404.html'), (error,data)=>{
+          response.end(data)
+        })
+      }else{
+        response.statusCode = 500
+        response.end('服务器繁忙，请稍后再试')
+      }
+    }else{
+      response.setHeader('Cache-Control', `public,max-age=${cacheTime}`)
+      response.end(data)
+    }
+  })
+
+  
 
 })
 
